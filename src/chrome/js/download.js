f314a4cb5682e58b38fd635113aa18facacbdb7b
Prakhar Gurunani <prakhargurunani@gmail.com>
2023-06-23 18:02:02 +0530

    fix: pdf conversion

--- src/chrome/js/download.js
+++ src/chrome/js/download.js
@@ -6,11 +6,19 @@ const downloadPDF = (event) => {
     let parentNode = buttonContainer.parentNode.parentNode.parentNode.parentNode.parentNode
     let messageContent = parentNode.querySelector(".model-response-text");
 
+    // Fix text color
+    // Fetch query name from DOM
     var doc = new jsPDF();
     doc.html(messageContent, {
         callback: function (pdf) {
-            pdf.save("DOC.pdf");
+            pdf.save("bard2pdf.pdf");
         },
+        margin: [10, 10, 10, 10],
+        autoPaging: "text",
+        x: 15,
+        y: 15,
+        width: 170,
+        windowWidth: 650,
     });
 }
 
