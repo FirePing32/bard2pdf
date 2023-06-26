4c634429b94e15ba4a0e4ba9bcdf489769bf7663
Prakhar Gurunani <prakhargurunani@gmail.com>
2023-06-26 21:30:47 +0530

    fix: remove preload scripts

--- src/chrome/js/content-script.js
+++ src/chrome/js/content-script.js
@@ -1,5 +1,9 @@
 "use strict";
 (async () => {
+  var preloadScripts = document.querySelectorAll("link, script");
+  for (let i = 0; i < preloadScripts.length; i++) {
+      preloadScripts[i].setAttribute("data-html2canvas-ignore", "true");
+  }
   const src = chrome.runtime.getURL("js/content-main.js");
   const contentMain = await import(src);
 })();
--- src/chrome/js/download.js
+++ src/chrome/js/download.js
@@ -2,22 +2,24 @@
 import { jsPDF } from "jspdf";
 
 const downloadPDF = (event) => {
-    let buttonContainer = event.target
-    let parentNode = buttonContainer.parentNode.parentNode.parentNode.parentNode.parentNode
+    let buttonContainer = event.target;
+    let parentNode =
+        buttonContainer.parentNode.parentNode.parentNode.parentNode.parentNode;
     let messageContent = parentNode.querySelector(".model-response-text");
 
-    // Fix script load
     var doc = new jsPDF();
     doc.html(messageContent, {
         callback: function (pdf) {
             pdf.output("dataurlnewwindow");
         },
-        html2canvas: {onclone: (clonedDoc) => {
-            const style = clonedDoc.createElement("style");
-            style.innerHTML =
-                "* { color: #000000 !important; }";
-            clonedDoc.body.appendChild(style);
-        }},
+        html2canvas: {
+            onclone: (clonedDoc) => {
+                const style = clonedDoc.createElement("style");
+                style.innerHTML =
+                    "* { color: #000000 !important; }";
+                clonedDoc.body.appendChild(style);
+            }
+        },
         margin: [10, 10, 10, 10],
         autoPaging: "text",
         x: 15,
@@ -25,6 +27,6 @@ const downloadPDF = (event) => {
         width: 170,
         windowWidth: 650,
     });
-}
+};
 
-export {downloadPDF};
+export { downloadPDF };
